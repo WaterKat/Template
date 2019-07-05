7e89923f6aec73dfd7f522360e2ecf0bf2aec5da
WaterKat <waterkatgames@gmail.com>
2019-07-04 17:42:06 -0700

    Adding AudioTrimmingGUI

--- Assets/WaterKat/AudioManager/Editor/TestEditor.cs
+++ Assets/WaterKat/AudioManager/Editor/TestEditor.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using UnityEngine.UIElements;
+using Unity.Collections;
 
 namespace WaterKat.Audio
 {
@@ -9,16 +11,12 @@ namespace WaterKat.Audio
         AudioClip SourceClip
         {
             get { return sourceClip; }
-            set { if (value == sourceClip) { return; } sourceClip = value; UpdateAudioTexture(); }
+            set { if (value == sourceClip) { return; } sourceClip = value; miniDisplay = UnzoomedSpectrographTexture(sourceClip, 40); ; }
         }
         AudioClip ExportClip;
 
-        Texture CurrentAudioTexture;
-
-        string myString = "Hello World";
-        bool groupEnabled;
-        bool myBool = true;
-        float myFloat = 1.23f;
+        Texture miniDisplay;
+        Texture darkenedAudioTexture;
 
         // Add menu named "My Window" to the Window menu
         [MenuItem("WaterKat/AudioEditor", false, 50)]
@@ -27,39 +25,173 @@ namespace WaterKat.Audio
             // Get existing open window or if none, make a new one:
             AudioEditor window = (AudioEditor)EditorWindow.GetWindow(typeof(AudioEditor));
             window.Show();
-        }
-
-        void OnGUI()
-        {
-            GUILayout.Label("General", EditorStyles.boldLabel);
-            SourceClip = EditorGUILayout.ObjectField("Source Audio", SourceClip, typeof(AudioClip), false) as AudioClip;
-
-            GUILayout.Label(CurrentAudioTexture,new GUILayoutOption[] { GUILayout.ExpandHeight(true),GUILayout.ExpandWidth(true)});
-
-            ExportClip = EditorGUILayout.ObjectField("Export Audio", ExportClip, typeof(AudioClip), false) as AudioClip;
-
-            myString = EditorGUILayout.TextField("Text Field", myString);
-
-            groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
-            myBool = EditorGUILayout.Toggle("Toggle", myBool);
-            myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
-            EditorGUILayout.EndToggleGroup();
+            
         }
 
         void UpdateAudioTexture()
         {
-            CurrentAudioTexture = SpectrographTexture(SourceClip);
+            miniDisplay = UnzoomedSpectrographTexture(sourceClip, 16);
+            darkenedAudioTexture = UpdateDarkenedTexture(SpectrographTexture(sourceClip, 4));
+        }
+        
+        class Slider {
+            public float minVal = 0;
+            public float minLimit = 0;
+            public float maxVal = 100;
+            public float maxLimit = 100;
+            public float originalminLimit;
+            public float originalmaxLimit;
+
+            public Slider() { LogMinMaxLimit(); }
+            public void LogMinMaxLimit()
+            {
+                originalminLimit = minLimit;
+                originalmaxLimit = maxLimit;
+            }
         }
 
-        Texture SpectrographTexture(AudioClip audioClip)
+        Slider ZoomSlider = new Slider();
+        Slider TrimSlider = new Slider();
+        Vector2 OldScreenSize = Vector2.zero;
+
+        void OnGUI()
         {
-            int width = 320;
-            int height = 200;
+            if ((OldScreenSize.x != position.width)|| (OldScreenSize.y != position.height))
+            {
+                UpdateScreen();
+                OldScreenSize = new Vector2(position.width, position.height);
+            }
+            GUILayout.Label("AudioViewer", EditorStyles.boldLabel);
+            SourceClip = EditorGUILayout.ObjectField("Source Audio", SourceClip, typeof(AudioClip), false) as AudioClip;
+
+            EditorGUILayout.LabelField("Zoom", EditorStyles.boldLabel);
+            GUILayout.Box(miniDisplay, new GUIStyle() { margin = new RectOffset(), fixedHeight = miniDisplay.height, fixedWidth = miniDisplay.width }, new GUILayoutOption[] { });
+
+            { //This controls the ZoomSlider
+                float min = ZoomSlider.minVal;
+                float max = ZoomSlider.maxVal;
+                EditorGUILayout.MinMaxSlider(ref ZoomSlider.minVal, ref ZoomSlider.maxVal, ZoomSlider.minLimit, ZoomSlider.maxLimit);
+                if ((ZoomSlider.minVal != min) || (ZoomSlider.maxVal != max))
+                {
+                    TrimSlider.minLimit = ZoomSlider.minVal;
+                    TrimSlider.maxLimit = ZoomSlider.maxVal;
+                }
+            }
+
+            EditorGUILayout.LabelField("Trim", EditorStyles.boldLabel);
+
+            GUILayout.Box(darkenedAudioTexture, new GUIStyle() { margin = new RectOffset(), fixedHeight = position.width / 4, fixedWidth = position.width }, new GUILayoutOption[] { });
+
+            {
+                float min = TrimSlider.minVal;
+                float max = TrimSlider.maxVal;
+                EditorGUILayout.MinMaxSlider(ref TrimSlider.minVal, ref TrimSlider.maxVal, TrimSlider.minLimit, TrimSlider.maxLimit);
+                if ((ZoomSlider.minVal != min)|| (ZoomSlider.maxVal != max))
+                {
+                    //darkenedAudioTexture = SpectrographTexture(sourceClip, 4);
+                    darkenedAudioTexture = UpdateDarkenedTexture(SpectrographTexture(sourceClip, 4));
+                }
+
+            }
+
+
+            ExportClip = EditorGUILayout.ObjectField("Export Audio", ExportClip, typeof(AudioClip), false) as AudioClip;
+            GUILayout.Button("Export!");
+    
+        }
+        void UpdateScreen()
+        {
+            UpdateAudioTexture();
+        }
+
+        
+        Texture UpdateDarkenedTexture(Texture inputTexture)
+        {
+            if (inputTexture == null) { return null; }
+            int width = (int)inputTexture.width;
+            int height = (int)inputTexture.height;
+            Texture2D audioTexture = new Texture2D(width,height, TextureFormat.ARGB32, false);
+            Graphics.CopyTexture(inputTexture, audioTexture);
+            float minPixel = (TrimSlider.minVal-TrimSlider.minLimit)/(TrimSlider.maxLimit-TrimSlider.minLimit)* width;
+            float maxPixel = (TrimSlider.maxVal - TrimSlider.minLimit) / (TrimSlider.maxLimit - TrimSlider.minLimit) * width;
+
+            for (int x = 0; x < width; x++)
+            {
+                if ((x < minPixel) || (x > maxPixel))
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        Color newColor = audioTexture.GetPixel(x, y);
+                        newColor /= 2;
+                        newColor.a = 1;
+                        audioTexture.SetPixel(x, y, newColor);
+                    }
+                }
+            }
+
+            audioTexture.Apply();
+            return audioTexture;
+        }
+
+        Texture UnzoomedSpectrographTexture(AudioClip audioClip, int heightRatio)
+        {
+            int width = (int)position.width;
+            int height = (int)position.width / heightRatio;
             int originOffset = height / 2;
             Texture2D audioTexture = new Texture2D(width, height, TextureFormat.ARGB32, false);
 
             if (audioClip == null) { return audioTexture; }
+            /*
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    audioTexture.SetPixel(x, y, new Color(0, 0, 0));
+                }
+            }
+            */
+            float[] audioData = new float[audioClip.samples];
+            audioClip.GetData(audioData, 0);
 
+            for (int x = 0; x < width; x++)
+            {
+                //                int targetSample = Mathf.FloorToInt((x* audioData.Length) /width);
+                //float startIndex = ZoomSlider.minVal / (ZoomSlider.maxLimit - ZoomSlider.minLimit) * audioData.Length;
+                //float endIndex = ZoomSlider.maxVal / (ZoomSlider.maxLimit - ZoomSlider.minLimit) * audioData.Length;
+                float startIndex = 0;
+                float endIndex = audioData.Length;
+                float relativeIndex = (float)x / (float)width;
+                int currentIndex = Mathf.FloorToInt(((1 - relativeIndex) * startIndex) + (relativeIndex * endIndex));
+
+
+                //int targetSample = Mathf.FloorToInt((TrimSlider.minVal / TrimSlider.maxLimit)*audioData.Length)+Mathf.FloorToInt(((float)x/width)*TrimmedRange);
+                //Debug.Log("TargetSample" + targetSample);
+                float scaledSample = audioData[currentIndex] * originOffset;
+
+                for (int i = 0; Mathf.Abs(i) < Mathf.Abs(scaledSample); i += (int)Mathf.Sign(scaledSample))
+                {
+                    audioTexture.SetPixel(x, Mathf.Clamp(i + originOffset, 0, height), new Color(1, 0, 0));
+                }
+
+                audioTexture.SetPixel(x, (int)scaledSample + originOffset, new Color(1, 0, 0));
+
+            }
+
+
+
+            audioTexture.Apply();
+            return audioTexture;
+        }
+
+        Texture SpectrographTexture(AudioClip audioClip,int heightRatio)
+        {
+            int width = (int)position.width;
+            int height = (int)position.width/heightRatio;
+            int originOffset = height / 2;
+            Texture2D audioTexture = new Texture2D(width, height, TextureFormat.ARGB32, false);
+
+            if (audioClip == null) { return audioTexture; }
+            /*
             for (int x = 0; x < width; x++)
             {
                 for (int y = 0; y < height; y++)
@@ -69,22 +201,30 @@ namespace WaterKat.Audio
                         audioTexture.SetPixel(x, y, new Color(0.70f, 0.35f, 0.07f));
                         continue;
                     }
-                    if (Mathf.Repeat(x,50) == 0)
+                    if (Mathf.Repeat(x,10) == 0)
                     {
                         audioTexture.SetPixel(x, y, new Color(1.00f, 0.60f, 0.27f));
                         continue;
                     } 
                     audioTexture.SetPixel(x, y, new Color(1.00f, 0.70f, 0.46f));
                 }
-            }
+            }*/
             
             float[] audioData = new float[audioClip.samples];
-            audioClip.GetData(audioData, 4000);
+            audioClip.GetData(audioData, 0);
             
             for (int x = 0; x < width; x++)
             {
-                int targetSample = Mathf.FloorToInt((x* audioData.Length) /width);
-                float scaledSample = audioData[targetSample]*originOffset;
+                //                int targetSample = Mathf.FloorToInt((x* audioData.Length) /width);
+                float startIndex =ZoomSlider.minVal / (ZoomSlider.maxLimit - ZoomSlider.minLimit) * audioData.Length;
+                float endIndex = ZoomSlider.maxVal / (ZoomSlider.maxLimit - ZoomSlider.minLimit) * audioData.Length;
+                float relativeIndex = (float)x / (float)width;
+                int currentIndex = Mathf.FloorToInt(((1 - relativeIndex) * startIndex) + (relativeIndex * endIndex));
+
+
+                //int targetSample = Mathf.FloorToInt((TrimSlider.minVal / TrimSlider.maxLimit)*audioData.Length)+Mathf.FloorToInt(((float)x/width)*TrimmedRange);
+                //Debug.Log("TargetSample" + targetSample);
+                float scaledSample = audioData[currentIndex]*originOffset;
 
                 for (int i = 0; Mathf.Abs(i) < Mathf.Abs(scaledSample); i+=(int)Mathf.Sign(scaledSample))
                 {
--- Assets/WaterKat/BezierCurves/Knob.cs
+++ Assets/WaterKat/BezierCurves/Knob.cs
@@ -8,34 +8,35 @@ namespace WaterKat.Benzier
     {
         Vector3 position;
         public Vector2 Position { get; set; }
-        Vector2 knobA;
-        public Vector2 KnobA
+        Vector2 _inKnob;
+        public Vector2 inKnob
         {
             get
             {
-                return knobA;
+                return _inKnob;
             }
             set
             {
                 Vector2 vector2 = value;
                 vector2.x = Mathf.Min(vector2.x, position.x);
-                knobA = vector2;
+                _inKnob = vector2;
             }
         }
-        Vector2 knobB;
-        Vector2 KnobB
+        Vector2 _outKnob;
+        public Vector2 outKnob
         {
             get
             {
-                return knobB;
+                return outKnob;
             }
             set
             {
                 Vector2 vector2 = value;
                 vector2.x = Mathf.Max(vector2.x, position.x);
-                knobB = vector2;
+                outKnob = vector2;
             }
         }
+        bool Smooth;
         bool Symmetric;
 
         // Start is called before the first frame update
@@ -48,6 +49,23 @@ namespace WaterKat.Benzier
         void Update()
         {
 
+        }
+    }
+
+    public class BezierCurve
+    {
+        Node2D nodeA;
+        Node2D nodeB;
+
+        float evaluateFloat(float t,float x0, float x1,float x2,float x3)
+        {
+           return (Mathf.Pow((1 - t), 3) * x0) + (3 * Mathf.Pow((1 - t), 2) * t * x1) + (3 * (1 - t) * Mathf.Pow(t, 2) * x2) + (Mathf.Pow(t, 3) * x3);
+        }
+        public float Evaluate(float t)
+        {
+            //float xf = evaluateFloat(t, nodeA.Position.x, nodeA.outKnob.x, nodeB.inKnob.x, nodeB.Position.x);
+            float yf = evaluateFloat(t, nodeA.Position.y, nodeA.outKnob.y, nodeB.inKnob.y, nodeB.Position.y);
+            return yf;
         }
     }
 }
