12d438a5d9d9e5db383898c9e87c71548517ccdf
WaterKat <waterkatgames@gmail.com>
2019-10-06 09:04:05 -0700

    Creating JumpTemplate

---
+++ Assets/WaterKat/WKPlayer/Scripts/Editor/JumpEditor.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace WaterKat.WKPlayer
+{
+    [CustomEditor(typeof(Jump))]
+    public class JumpEditor : Editor
+    {
+        SerializedProperty calculationMode;
+
+        SerializedProperty Gravity;
+        SerializedProperty JumpHeight;
+        SerializedProperty JumpTime;
+
+        void OnEnable()
+        {
+            calculationMode = serializedObject.FindProperty("calculationMode");
+
+            Gravity = serializedObject.FindProperty("Gravity");
+            JumpHeight = serializedObject.FindProperty("JumpHeight");
+            JumpTime = serializedObject.FindProperty("JumpTime");
+
+
+
+        }
+
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("m_Script"));
+            EditorGUI.EndDisabledGroup();
+
+            EditorGUILayout.Space();
+
+            EditorGUILayout.PropertyField(calculationMode);
+
+            EditorGUILayout.Space();
+
+            switch (calculationMode.enumValueIndex)
+            {
+                case 0:
+                    EditorGUILayout.PropertyField(JumpHeight);
+                    EditorGUILayout.PropertyField(JumpTime);
+                    break;
+                case 1:
+                    EditorGUILayout.PropertyField(Gravity);
+                    EditorGUILayout.PropertyField(JumpHeight);
+                    break;
+                case 2:
+                    EditorGUILayout.PropertyField(Gravity);
+                    EditorGUILayout.PropertyField(JumpTime);
+                    break;
+            }
+
+                
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/WaterKat/WKPlayer/Scripts/Jump.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WaterKat.WKPlayer
+{
+    [RequireComponent(typeof(Collider))]
+    [RequireComponent(typeof(Rigidbody))]
+    public class Jump : MonoBehaviour
+    {
+        public enum CalculationModes
+        {
+            Height_Time = 0,
+            Gravity_Height = 1,
+            Gravity_Time = 2,
+        }
+
+        public CalculationModes calculationMode = CalculationModes.Height_Time;
+
+        public float Gravity = 10.0f;
+        public float JumpHeight = 5.0f;
+        public float JumpTime = 0.5f;
+
+        float JumpVelocity;
+
+        Rigidbody playerRigidBody;
+        Collider playerCollider;
+
+        private void Start()
+        {
+            playerRigidBody = GetComponent<Rigidbody>();
+            playerCollider = GetComponent<Collider>();
+
+            SetVelocity();
+        }
+
+        private void SetVelocity()
+        {
+            JumpVelocity = Gravity * JumpTime;
+        }
+
+        [ContextMenu("StartJump")]
+        private void StartJump()
+        {
+            playerRigidBody.velocity = Vector3.up * JumpVelocity * 2;
+        }
+
+        private void Update()
+        {
+            playerRigidBody.velocity += Vector3.down * Gravity * Time.deltaTime;
+        }
+    }
+}
