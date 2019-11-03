cf6b7c6b79c727a047eabd23810aec4fbe6487b2
WaterKat <waterkatgames@gmail.com>
2019-11-03 11:15:09 -0800

    Update

---
+++ Assets/WaterKat/MathW/Complex.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine;
+using System;
+
+namespace WaterKat.MathW
+{
+    [System.Serializable]
+    public struct ComplexF
+    {
+        public float real;
+        public float imaginary;
+
+        public ComplexF (float _real)
+        {
+            this.real = _real;
+            this.imaginary = 0;
+        }
+
+        public ComplexF(float _real,float _imaginary)
+        {
+            this.real = _real;
+            this.imaginary = _imaginary;
+        }
+
+        public static ComplexF From_Polar(float radius, float theta)
+        {
+            ComplexF data = new ComplexF(radius * (float)Math.Cos(theta*Constants.DegToRad), radius * (float)Math.Sin(theta * Constants.DegToRad));
+            return data;
+        }
+    }
+}
---
+++ Assets/WaterKat/MathW/Constants.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine;
+using System;
+
+namespace WaterKat.MathW
+{
+    public class Constants
+    {
+        public static double DegToRad
+        {
+            get
+            {
+                return (Math.PI / 180d);
+            }
+        }
+        public static double RadToDeg
+        {
+            get
+            {
+                return (180d / Math.PI);
+            }
+        }
+    }
+}
